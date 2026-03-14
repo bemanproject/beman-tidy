0b7c32110fffd5fb28be850d34a640a27d8be45f
Andrei Durlea <andrei.durlea.telefon@gmail.com>
2026-03-14 20:19:58 +0200

    Added more tests

---
+++ tests/lib/checks/beman_standard/cpp/namespace/valid/include/beman/my_lib/detail/some_dependency.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDED_BEMAN_MY_LIB_DETAIL_SOME_DEPENDENCY_H
+#define INCLUDED_BEMAN_MY_LIB_DETAIL_SOME_DEPENDENCY_H
+
+namespace beman::my_lib {
+namespace detail {
+
+struct some_dependency {};
+
+} // namespace detail
+} // namespace beman::my_lib
+
+#endif // INCLUDED_BEMAN_MY_LIB_DETAIL_SOME_DEPENDENCY_H
